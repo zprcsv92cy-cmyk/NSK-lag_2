3044f2c9e4981b6bbabded4953acfa0ab5fc3ce2
zprcsv92cy-cmyk <zprcsv92cy@privaterelay.appleid.com>
2026-02-28 21:52:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 'use strict';
 
 // Bump this when you deploy a new version
-const CACHE_VERSION = 'v63';
+const CACHE_VERSION = 'v64';
 const CACHE_NAME = `nsk-cache-${CACHE_VERSION}`;
 
 // Files to cache relative to the Service Worker scope (works for /repo/ and /)

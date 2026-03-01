6e886bbf7baf9a0b4a062b5b3fbc4c6ca6482b02
zprcsv92cy-cmyk <zprcsv92cy@privaterelay.appleid.com>
2026-03-01 14:37:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
      * Navigations (HTML): network-first (so GitHub updates come through)
      * Static assets: stale-while-revalidate
 */
-const CACHE = 'nsk-lag-cache-v72';
+const CACHE = 'nsk-lag-cache-v73';
 const CORE_ASSETS = [
   './',
   './index.html',
